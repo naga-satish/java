306d25d580bcaef5d285902dca08db3f446e45ad
naga-satish <57696040+naga-satish@users.noreply.github.com>
2020-01-28 09:35:10 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Multiplication {
+
+	public static void main(String[] args) {
+		System.out.print("Enter a number:");
+		int a,i,x;
+		Scanner in = new Scanner(System.in);
+		a=in.nextInt();
+		for(i=0;i<=12;i++)
+		{
+x=a*i;
+System.out.println(x);
+	}
+
+}
+}
